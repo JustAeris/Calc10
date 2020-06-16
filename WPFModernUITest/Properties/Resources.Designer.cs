a74c68fef7e3a933e07442aeabd24c7a845bbdd5
Astyr <matthieu043@outlook.fr>
2020-06-16 20:56:56 +0200

    Renaming the whole Project

--- WPFModernUITest/Properties/Resources.Designer.cs
+++ WPFModernUITest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFModernUITest.Properties {
+namespace Calc10.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WPFModernUITest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFModernUITest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Calc10.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
